d46d755d8f7988f361f89d33ff2ab6cb5c47a942
rmusayevr <rmusayevr19@gmail.com>
2022-02-25 14:33:08 +0400

    Hangman

---
+++ game.js
@@ -0,0 +1,66 @@
+var words = ["ant", "baboon", "badger", "bat", "bear", "beaver", "camel", "cat", "clam", "cobra", "cougar", "coyote", "crow", "deer", "dog", "donkey", "duck", "eagle", "ferret", "fox", "frog", "goat", "goose", "hawk", "lion", "lizard", "lama", "mole", "monkey", "moose", "mouse", "mule", "newt", "otter", "owl", "panda", "parrot", "pigeon", "python", "rabbit", "ram", "rat", "raven", "rhino", "salmon", "seal", "shark", "sheep", "skunk", "sloth", "snake", "spider", "stork", "swan", "tiger", "toad", "trout", "turkey", "turtle", "weasel", "whale", "wolf", "wombat", "zebra"];
+var wins = 0;
+var chances = 15;
+var guesses = [];
+
+var randomWord = words[Math.floor(Math.random() * words.length)];
+var text = "" 
+console.log(randomWord)
+for (var i = 0; i < randomWord.length; i++) {
+    text += "-";
+}
+document.querySelector("#word").innerHTML = "Current Word: " + text;
+function getAnswer() {
+    var a = document.querySelector("input").value.toLowerCase();
+    document.querySelector("#word").innerHTML = "Current Word: " + text;
+    checkAnswer(a);
+}
+function askAnswer(){
+    document.querySelector(".btn").addEventListener("click", getAnswer);
+}
+function checkAnswer(x) {
+    if (randomWord.includes(x)) {
+        for(var j = 0; j < randomWord.length; j++){
+            if(x === randomWord[j]){
+                // text = text.replace(text[j], randomWord[j])
+                text[j] = x;
+                console.log(text[j])
+                document.querySelector("#word").innerHTML = "Current Word: " + text;
+            }
+        }
+        if (text === randomWord) {
+            wins++;
+            document.querySelector("#wins").innerHTML = "Wins: " + wins;
+        }
+        else {
+            askAnswer();
+        }
+    }
+    else {
+        chances--;
+        document.querySelector("#numberOfGuess").innerHTML = "Number of Guesses Remaining: " + chances;
+        guesses.push(x);
+        document.querySelector("#lettersGuessed").innerHTML = "Letters Already Guessed: " +  guesses.join(" ");
+        if (chances == 0) {
+            alert("Your chances ran out! You Lost!")
+            restart();
+        }
+        else {
+            askAnswer();
+        }
+    }
+    document.querySelector("#input").value = "";
+}
+
+function restart() {
+    var randomWord = words[Math.floor(Math.random() * words.length)];
+    chances = 15
+    document.querySelector("#numberOfGuess").innerHTML = "Number of Guesses Remaining: " + chances;
+    guesses = [];
+    document.querySelector("#lettersGuessed").innerHTML = "Letters Already Guessed: " + guesses.join(" ");
+    text = ""
+    document.querySelector("#word").innerHTML = "Current Word: " + text;
+    askAnswer()
+}
+askAnswer()
+
